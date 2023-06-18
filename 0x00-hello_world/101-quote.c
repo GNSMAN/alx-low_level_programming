c9ee590355c4c743a513bac222cf513f064b7345
azedine <azoz10@gmail.com>
2023-06-18 09:38:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 /** 
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *  followed by a new line, to standard error.
- *  Return: Always 1.
+ * followed by a new line, to standard error.
+ * Return: Always 1.
  */
 
 
